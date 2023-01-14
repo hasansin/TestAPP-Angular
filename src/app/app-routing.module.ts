febfc0c00c55cee43d947a322298ac08afd272ec
hasansin <it19070704@my.sliit.lk>
2023-01-14 19:44:14 +0530

    navigator,signin form  added and edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { SignInComponent } from './sign-in/sign-in.component';
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:ServerComponent  },
+  { path: 'signin', component: SignInComponent },
+  {path:'',redirectTo:"/home",pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    SignInComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
